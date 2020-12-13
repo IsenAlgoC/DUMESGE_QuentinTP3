b48f5af1c53ce975b0865500ad3591b9c216cb55
Quentin DUMESGE <quentin.dumesge@student.yncrea.fr>
2020-12-14 00:49:03 +0100

    TP3

--- TPC3/Carre.c
+++ TPC3/Carre.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define carre(x) (x) * (x)   //il faut rajouter des parenthèses car Visual Studio ne les considère pas automatiquement
-
+#define carre(x) (x) * (x)	//Il faut rajouter des parenthèses car Visual Studio ne les considère pas automatiquement
+							//Sans parenthèse voici l'opération : carre(Nb+1) = Nb + 1 * Nb + 1 = Nb + Nb + 1 = 2*Nb + 1
 
 int main() {
 	int Nb = 5;
